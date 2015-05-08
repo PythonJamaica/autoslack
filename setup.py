486df875742429171d14280f263f7ab263ec6df2
David Bain <david@alteroo.com>
2015-05-08 00:51:13 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.0dev1',
+    version='0.0.1dev1',
 
     description='AutoSlack is a simple module designed to automate some tasks for http://slack.com.',
 
